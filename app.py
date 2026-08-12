873779d014b7b2c1927f33139a620cc097d364f8
prasannareddy0101-bit <prasannareddy0101@gmail.com>
2026-08-12 13:26:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,8 +123,7 @@ app = FastAPI(
 def root():
   return {"message" : "Server is running. Visit /agent/playground/ to chat, or /docs for the API."}
 
-
-app.add_route(app, formatted_agent_chain, path = "/agent")
+add_routes(app, formatted_agent_chain, path = "/agent")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
